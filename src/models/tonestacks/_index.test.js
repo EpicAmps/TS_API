297a628ab8af430b2593002f0f17fe3e2901d2e5
Epic Amps <53057421+EpicAmps@users.noreply.github.com>
2025-08-21 13:54:47 -0700

    Deleted _index.test.js

--- src/models/tonestacks/_index.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from 'vitest';
-import { topologies } from './_index';
-
-test('Tonestacks have unique ids', () => {
-  const flatTopologies = Object.values(topologies).flat();
-  const ids = flatTopologies.map((t) => t.id);
-  expect(ids.length).toBe(new Set(ids).size);
-});
